378c0ddee556f88546dbf2b81ca4671f4e25dc3f
shojol021 <shojol021@gmail.com>
2023-05-28 15:13:18 +0600

    delete data from cart

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 app.use(cors())
@@ -55,6 +55,13 @@ async function run() {
     app.post('/cart', async (req, res) => {
       const item = req.body;
       const result = await cartCollection.insertOne(item)
+      res.send(result)
+    })
+
+    app.delete('/cart/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
 
